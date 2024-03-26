81e23b5e39936da96004d3d0360830a73f12c8f2
Kevinli712390 <khl8545@nyu.edu>
2024-03-26 19:05:14 -0400

    scoreboard added

--- src/rockpaperscissors/game.py
+++ src/rockpaperscissors/game.py
@@ -1,24 +1,44 @@
 import random
 
-def determine_winner(player_choice , computer_choice):
+# Global scoreboard
+scoreboard = {'player': 0, 'computer': 0}
+
+def determine_winner(player_choice, computer_choice):
+    global scoreboard
     if player_choice == computer_choice:
-        return "It's a tie!"
+        result = "It's a tie!"
     elif player_choice == "rock":
         if computer_choice == "scissors":
-            return "You win!"
+            scoreboard['player'] += 1
+            result = "You win!"
         else:
-            return "Computer wins!"
+            scoreboard['computer'] += 1
+            result = "Computer wins!"
     elif player_choice == "paper":
         if computer_choice == "rock":
-            return "You win!"
+            scoreboard['player'] += 1
+            result = "You win!"
         else:
-            return "Computer wins!"
+            scoreboard['computer'] += 1
+            result = "Computer wins!"
     elif player_choice == "scissors":
         if computer_choice == "paper":
-            return "You win!"
+            scoreboard['player'] += 1
+            result = "You win!"
         else:
-            return "Computer wins!"
-        
+            scoreboard['computer'] += 1
+            result = "Computer wins!"
+    return result
+
 def generate_computer_choice():
-    choices = ["rock" , "paper" , "scissors"]
+    choices = ["rock", "paper", "scissors"]
     return random.choice(choices)
+
+def update_scoreboard(result):
+    print(result)
+    print(f"Scoreboard: Player {scoreboard['player']}, Computer {scoreboard['computer']}")
+
+def reset_scoreboard():
+    global scoreboard
+    scoreboard = {'player': 0, 'computer': 0}
+    print("Scoreboard has been reset.")
