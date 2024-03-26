bf9bff6cc2018a2651179adf173a8bf692414935
ZijieZha0 <zz3589@nyu.edu>
2024-03-26 19:31:34 -0400

    modified something for build&twine

---
+++ src/rock_paper_scissors_game/__main__.py
@@ -0,0 +1,8 @@
+from rock_paper_scissors_game.cli import play, simulate
+def main():
+    play()
+    print("Now simulating a game between two players...")
+    simulate()
+
+if __name__ == "__main__":
+    main()
